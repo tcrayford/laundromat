2e16b7f8b620f107525646c00b2a181376cc382c
Tom Crayford <tcrayford@googlemail.com>
2014-04-05 09:50:44 +0100

    fix race condition bug
    AssertionError is apparently not an exception. Thankfully it *is* at
    least throwable, so meh has to catch that as well

--- src/laundromat/concurrency.clj
+++ src/laundromat/concurrency.clj
@@ -3,73 +3,73 @@
             [clojure.test.check.properties :as prop]
             [laundromat.core :refer :all]))
 
+(defmacro meh [expr]
+  `(try
+     [::success ~expr]
+     (catch Throwable e#
+       [::failure e#])))
+
 (def shrinking-sentinel
   (gen/gen-pure
     [false [(gen/rose-pure true)]]))
 
-(defn debug [& args]
-  (.println *out*
-            (apply str args)))
-
 (defn run-command-concurrently [transitions [command-name args] symbolic-state actual-state]
-  (let [[actual-result-type resulting-actual] (try [::success (apply (get-in transitions [command-name :command]) actual-state args)]
-                 (catch Exception e
-                   [::failure e]))
-        [symbolic-result-type resulting-symbolic] (try [::success (swap! symbolic-state #((get-in transitions [command-name :next]) % args))]
-                                                    (catch Exception e
-                                                      [::failure e]))]
+  (let [[actual-result-type resulting-actual] (meh (apply (get-in transitions [command-name :command]) actual-state args))
+        [symbolic-result-type resulting-symbolic] (meh (swap! symbolic-state #((get-in transitions [command-name :next]) % args)))]
     (if (and (= actual-result-type ::success) (= symbolic-result-type ::success))
       (let [[postcondition-success postcondition-result]
-            (try
-              [::success ((get-in transitions [command-name :postcondition] (constantly true)) resulting-actual resulting-symbolic args)]
-              (catch Exception e
-                [::failure e]))]
+            (meh ((get-in transitions [command-name :postcondition] (constantly true)) resulting-actual resulting-symbolic args))]
         (if (= postcondition-success ::success)
           {:symbolic-state resulting-symbolic
            :result resulting-actual
            :success true}
-        {:failure (str "postcondition failed " postcondition-success " " postcondition-result)}))
-      {:failure [resulting-actual resulting-symbolic]})))
+          (do
+            (.println *out* (str postcondition-result))
+            {:failure (str "postcondition failed " postcondition-success " " postcondition-result)})))
+      {:failure [actual-result-type resulting-actual symbolic-result-type resulting-symbolic args]})))
 
 (defn my-deliver [finished worker-id x test-run-n]
-  (debug (str "about to deliver on " test-run-n "/" worker-id " " x))
   (deliver (nth finished worker-id) x))
 
 (defonce test-run-count (atom 0))
 
+(defn run-worker [test-run-n worker-id workers-commands transitions symbolic-state actual-state finished]
+  (future
+    (try
+      (reduce
+        (fn [{is-finished :finished result :result :as r} current-command]
+          (if is-finished
+            r
+            (let [result (run-command-concurrently transitions current-command symbolic-state actual-state)]
+              (if (:success result)
+                (assoc r :result nil)
+                (do
+                  (.println *out* (str result " " test-run-n))
+                  (my-deliver finished worker-id result test-run-n)
+                  (assoc r :finished true))))))
+        {:finished false}
+        (workers-commands worker-id))
+        (my-deliver finished worker-id true test-run-n)
+      (catch Exception e
+        (.printStackTrace e)
+        (my-deliver finished worker-id e test-run-n)))))
+
 (defn run-and-check-commands-concurrently [workers-commands transitions concurrency]
   (let [test-run-n      (swap! test-run-count inc)
-        finished        (map (fn [_] (promise)) (range concurrency))
+        finished        (into [] (map (fn [_] (promise)) (range concurrency)))
         symbolic-state  (atom ((get-in transitions [:initial-state :initial])))
         actual-state    ((get-in transitions [:initial-state :subject]))]
     (dotimes [worker-id concurrency]
-      (future
-        (debug "starting work on " test-run-n "/" worker-id)
-        (try
-          (loop [current-command (first (workers-commands worker-id))
-                 remaining       (rest  (workers-commands worker-id))]
-            (debug "running command " current-command " on " (str test-run-n "/" worker-id))
-            (if current-command
-              (let [result (run-command-concurrently transitions current-command symbolic-state actual-state)]
-                (if (:success result)
-                  (recur (first remaining)
-                         (rest remaining))
-                  (my-deliver finished worker-id result test-run-n)))
-              (my-deliver finished worker-id true test-run-n)))
-          (catch Exception e
-            (debug e)
-            (my-deliver finished worker-id e test-run-n)))
-        (debug "finished on " test-run-n "/" worker-id)))
+      (run-worker test-run-n worker-id workers-commands transitions symbolic-state actual-state finished))
     (doall
       (map
         (fn [n x]
-          (println "about to deref on " test-run-n "/" x n)
-          (let [r (deref n 300 ::timeout)]
+          (let [r (deref n 300 [::timeout x])]
             (if (= true r)
               r
               (if (isa? Exception r)
                 (throw r)
-                (assert false (str r " " test-run-n "/" x))))))
+                (assert false r)))))
         finished
         (range concurrency)))
     true))
@@ -82,22 +82,21 @@
       (if (and (= result-type ::success)
                (not (= left 0)))
         (recur (dec left))
-        (throw actual-result)))))
+        (if (= left 0)
+          (if (= result-type ::success)
+            actual-result
+            (throw actual-result)))))))
 
 (defn run-and-repeat-for-races [workers-commands transitions concurrency is-shrinking]
   (let [[result-type actual-result]
-        (try
-          [::success (run-and-check-commands-concurrently workers-commands transitions concurrency)]
-          (catch Exception e
-            [::failure e]))]
+        (meh
+          (run-and-check-commands-concurrently workers-commands transitions concurrency))]
     (if (and is-shrinking (= result-type ::success))
       (run-until-failure-or-stop
         times-to-repeat-for-races
         (fn []
-          (try
-            [::success (run-and-check-commands-concurrently workers-commands transitions concurrency)]
-            (catch Exception e
-              [::failure e]))))
+          (meh
+            (run-and-check-commands-concurrently workers-commands transitions concurrency))))
       (if (= result-type ::success)
         true
         (if (isa? Exception actual-result)
