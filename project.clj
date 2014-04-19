ee4cc648fa1996eb2441c67f2a6f300779932783
Tom Crayford <tcrayford@googlemail.com>
2014-04-19 21:19:20 +0100

    fuck NullPointerExceptions with no stack trace

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject laundromat "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
